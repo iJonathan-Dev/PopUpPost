844c80c0babf854246d5c7bba2100138111bf0cd
iJonathan-Dev <ignatius.jonathan@outlook.com>
2024-11-11 18:44:30 +1300

    add hide pop-up when read more is clicked

--- Emergency.js
+++ Emergency.js
@@ -1,38 +1,38 @@
-let categoryId;
+let CategoryId;
 
-const slug = "Announcements";
+const Slug = "Announcements";
 
 const elementId = "Announcements";
 
 const Hide = () => {
   const element = document.getElementById(elementId);
   element.style.display = "none";
-  console.log("element is hidden");
+  //console.log("element is hidden");
 };
 
 const StoreSession = () => {
   Hide();
   sessionStorage.setItem("elementHidden", "true");
-  console.log("Post is hidden during this session");
+  //console.log("Post is hidden during this session");
 };
 
 const Show = () => {
   const element = document.getElementById(elementId);
   element.style.display = "block";
-  console.log("element is shown");
+  //console.log("element is shown");
 };
 
+//check if there's any post with that categories ID, show the pop-up box if yes
 const checkPost = () => {
-  fetch(`/wp-json/wp/v2/posts?categories=${categoryId}`)
+  fetch(`/wp-json/wp/v2/posts?categories=${CategoryId}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.length > 0 && sessionStorage.getItem("elementHidden") !== "true") {
-        console.log("There are posts with the specified category.");
+        //console.log("There are posts with the specified category.");
         Show();
-        console.log(data[0].title.rendered);
+        //console.log(data[0].title.rendered);
       } else {
-        console.log("No posts found in the specified category.");
-        // Do something else if no posts are found
+        //console.log("No posts found in the specified category.");
         Hide();
       }
     })
@@ -41,13 +41,19 @@ const checkPost = () => {
 
 Hide();
 
-fetch(`/wp-json/wp/v2/categories?slug=${slug}`)
+//check if Post with specified categories(slug) exist, then get the category ID & run checkPost function
+fetch(`/wp-json/wp/v2/categories?slug=${Slug}`)
   .then((response) => response.json())
   .then((data) => {
-    categoryId = data[0].id;
-    console.log(categoryId);
+    CategoryId = data[0].id;
+    //console.log(CategoryId);
     checkPost();
   })
   .catch((error) => console.error("Error getting Slug ID:", error));
 
+//Hide post when user click the close button
 document.getElementById("closeBtn").onclick = StoreSession;
+
+//Hide post when user click "Read More" on the post
+const PostReadMore = document.getElementById("contentPost").querySelector(".elementor-post__read-more");
+PostReadMore.onclick = StoreSession;
